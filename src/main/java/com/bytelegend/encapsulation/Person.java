da6a396d44a05260e728421234d541b405532844
saveunhappy <925147954@qq.com>
2022-05-15 13:58:26 +0000

    Update at 2022-05-15T13:58:26.757339Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -38,7 +38,7 @@ public class Person {
     }
 
     public String getName() {
-        return getFirstName() + getLastName();
+        return getFirstName() + " " + getLastName();
     }
 
     public void setName(String name) {
