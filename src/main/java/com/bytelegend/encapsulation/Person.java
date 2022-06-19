9a45aa31061e750de33a8008fe9f9507b25499e4
SpaceTimeAdministrator <106767177+SpaceTimeAdministrator@users.noreply.github.com>
2022-06-19 09:12:14 +0000

    Update at 2022-06-19T09:12:14.783104Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -10,11 +10,21 @@ package com.bytelegend.encapsulation;
 public class Person {
     /** The name of the person. */
     private String name;
+
+    private String firstName;
+    private String lastName;
     /** The age of the person. */
     private int age;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
 
     public String getName() {
-        return name;
+        return firstName + " " + lastName;
     }
 
     public void setName(String name) {
