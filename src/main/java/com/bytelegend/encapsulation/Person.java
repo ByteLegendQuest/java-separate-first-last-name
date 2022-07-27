73bce2217925e66edcf395085ba70bf353da23fa
greek-zzf <49605925+greek-zzf@users.noreply.github.com>
2022-07-27 03:43:58 +0000

    Update at 2022-07-27T03:43:58.427805Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -8,13 +8,25 @@ package com.bytelegend.encapsulation;
  * `getName()` method - nothing else needs to be changed.
  */
 public class Person {
-    /** The name of the person. */
+    /**
+     * The name of the person.
+     */
     private String name;
-    /** The age of the person. */
+    /**
+     * The age of the person.
+     */
     private int age;
+    /**
+     * The firstName of the person.
+     */
+    private String firstName;
+    /**
+     * The lastName of the person.
+     */
+    private String lastName;
 
     public String getName() {
-        return name;
+        return firstName + " " + lastName;
     }
 
     public void setName(String name) {
@@ -27,5 +39,21 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
     }
 }
