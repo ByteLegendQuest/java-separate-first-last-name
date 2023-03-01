0017f4256c875c807fb4e75539a6a1a40868da53
ov0o0vo <3035566793@qq.com>
2023-03-01 02:29:32 +0000

    Update at 2023-03-01T02:29:32.924023Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -12,15 +12,21 @@ public class Person {
     private String name;
     /** The age of the person. */
     private int age;
-
+    private String firstName;
+    private String lastName;
     public String getName() {
-        return name;
+        return firstName + " " + lastName;
     }
 
     public void setName(String name) {
         this.name = name;
     }
-
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+    public void setLastName(String name) {
+        this.lastName = name;
+    }
     public int getAge() {
         return age;
     }
