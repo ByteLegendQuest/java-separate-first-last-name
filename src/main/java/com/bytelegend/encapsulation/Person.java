8516c6ff96aa05194c25a913f5b7844bc9468e23
DasBrain <johannes13@johannes13.de>
2023-05-20 18:38:46 +0000

    Update at 2023-05-20T18:38:46.550882Z

--- src/main/java/com/bytelegend/encapsulation/Person.java
+++ src/main/java/com/bytelegend/encapsulation/Person.java
@@ -9,7 +9,8 @@ package com.bytelegend.encapsulation;
  */
 public class Person {
     /** The name of the person. */
-    private String name;
+    private String firstName;
+    private String lastName;
     /** The age of the person. */
     private int age;
 
@@ -17,8 +18,12 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
     }
 
     public int getAge() {
